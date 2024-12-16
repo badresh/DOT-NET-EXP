9f4d05398ba82d7dc0ba919a020d8a5207d235f1
BadreshB <114792662+badresh@users.noreply.github.com>
2024-12-16 15:59:15 +0530

    Add files via upload

---
+++ C#.NET/Exception Handling EX 4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+class ExceptionHandling4
+{
+    static void Main()
+    {
+        try
+        {
+            Console.WriteLine("Enter a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int result = 10 / number;
+            Console.WriteLine("Result: " + result);
+        }
+        catch (FormatException ex)
+        {
+       
+            Console.WriteLine("Format Exception: " + ex.Message);
+        }
+        catch (DivideByZeroException ex)
+        {
+            
+            Console.WriteLine("Divide By Zero Exception: " + ex.Message);
+        }
+        catch (Exception ex)
+        {
+   
+            Console.WriteLine("Exception: " + ex.Message);
+        }
+        finally
+        {
+          
+            Console.WriteLine("Finally block executed.");
+        }
+    }
+}
